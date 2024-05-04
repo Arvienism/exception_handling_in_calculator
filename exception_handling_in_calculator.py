7bb26722c2b5c758929dae7e403c0968f92dc743
Arvie Lastra <lastraarvie@gmail.com>
2024-05-05 02:44:34 +0800

    Added pseudocode

---
+++ exception_handling_in_calculator.py
@@ -0,0 +1,52 @@
+# start: 
+#   define a menu function to display the calculator menu
+#       display "Calculator Menu"- add some astig effect!
+#       display "1. Addition"
+#       display "2. Substraction"
+#       display "3. Multiplication"
+#       display "4. Division"
+#       display "5. EXIT the program"
+#   define Addition function to solve for addition
+#       Test a block of code for errors
+#           input the first number
+#           input the second number
+#           calculation for addition - add the first and second number
+#           display the result for addition
+#       Handle errors for ValueError
+#           tell the user that they inputted wrong data type
+#   define Subtraction function to solve for substraction
+#       Test a block of code for errors
+#           input the first number
+#           input the second number
+#           calculation for substraction - substract the first and second number
+#           display the result for substraction
+#       Handle errors for ValueError
+#   define Multiplication function to solve for multiplication
+#       Test a block of code for errors
+#           input the first number
+#           input the second number
+#           calculation for multiplication - multiply the first and second number
+#           display the result for multiplication
+#       Handle errors for ValueError
+#   define Division function to solve for division
+#       Test a block of code for errors
+#           input the first number
+#           input the second number
+#           calculation for division - divide the first and second number
+#           display the result for division 
+#       Handle errors for ValueError
+#       Handle errors for ZeroDivisionError
+#   define main function to run the calculator
+#       Test a block of code for errors
+#           create an infinite loop to let the user to continue using the calculator
+#               call the menu function
+#               input the chosen number in the menu function
+#               call Addition function if the user chosen number is 1
+#               call Substraction function if the user chosen number is 2
+#               call Multiplication function if the user chosen number is 3
+#               call Division function if the user chosen number is 4
+#               call Exit function if the user chosen number is 5
+#               Ask user if they want to try again
+#               display "Thank you!"
+#       Handle errors for ValueError
+# end:
